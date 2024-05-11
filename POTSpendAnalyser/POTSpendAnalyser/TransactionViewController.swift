ca550f780e802d9a1aeedc89be74661b486a9601
Shanthakumar Vm <sk@Shanthakumars-MacBook-Pro-2.local>
2024-05-11 14:43:49 +0530

    Added view controllers

---
+++ POTSpendAnalyser/POTSpendAnalyser/TransactionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TransactionViewController.swift
+//  POTSpendAnalyser
+//
+//  Created by Shanthakumar Vm on 11/05/24.
+//
+
+import UIKit
+
+class TransactionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
